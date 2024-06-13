fdf3bb29592088d708379182d5ba3567b4f2f57c
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    platina: Build missing library for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- device.mk
+++ device.mk
@@ -43,7 +43,8 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
-    libmegface
+    libmegface \
+    libpng.vendor:32
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libpiex.so:$(TARGET_COPY_OUT_VENDOR)/lib/libpiex-v29.so \
