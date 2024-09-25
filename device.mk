b8bf2c8037a03edb16495e08648a056690404bed
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    platina: Switch to AIDL USB HAL
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- device.mk
+++ device.mk
@@ -86,7 +86,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb-service.qti
 
 # Wifi
 PRODUCT_PACKAGES += \
