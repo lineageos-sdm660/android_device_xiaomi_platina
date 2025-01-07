04257fd2e8b761114bc4b69ffb918efedb0d4cb4
LuK1337 <priv.luk@gmail.com>
2025-01-07 10:47:00 +0100

    platina: Clear LIBC_PRIVATE symbol versions for libmmcamera_faceproc
    Change-Id: I1565a6fef5070377650e62dc3e3a45bfd2bfaf5b

--- extract-files.py
+++ extract-files.py
@@ -29,6 +29,10 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'\xdb\xf8\x00\x10\x08\x9a\x49\x6b\xc6\xe9\x04\x21', b'\xdb\xf8\x00\x10\x08\x9a\x89\x6c\xc6\xe9\x04\x21'),
     'vendor/lib/libMiCameraHal.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
+    'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
+        .clear_symbol_version('__aeabi_memcpy')
+        .clear_symbol_version('__aeabi_memset')
+        .clear_symbol_version('__gnu_Unwind_Find_exidx'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
